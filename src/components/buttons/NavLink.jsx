38728559537f08dc2b24435c63db88944488811f
Badsha012 <badshasheikh361@gmail.com>
2026-01-20 22:25:03 +0600

    update a navabr

--- src/components/buttons/NavLink.jsx
+++ src/components/buttons/NavLink.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+"use client";
 
-const NavLink = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const NavLink = ({ href, children }) => {
+  const pathname = usePathname();
+
+  const isActive = pathname === href;
+
+  return (
+    <Link
+      href={href}
+      className={isActive ? "text-primary font-bold" : "text-gray-600"}
+    >
+      {children}
+    </Link>
+  );
 };
 
 export default NavLink;
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,11 +1,28 @@
+
+"use client";
 import React from "react";
 import Logo from "./Logo";
+import NavLink from "../buttons/NavLink";
 
 const Navbar = () => {
 const nav=<>
 <li>
-  
+  <NavLink href={"/"}>Home</NavLink>
+
 </li>
+<li>
+  <NavLink href={"/products"}>Products</NavLink>
+
+</li>
+<li>
+  <NavLink href={"/blog"}>Blog</NavLink>
+
+</li>
+<li>
+  <NavLink href={"/contact"}>Contact</NavLink>
+
+</li>
+
 </>
 
 
@@ -35,48 +52,14 @@ const nav=<>
               tabIndex="-1"
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {nav}
             </ul>
           </div>
          <Logo></Logo>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2 bg-base-100 w-40 z-1">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+           {nav}
           </ul>
         </div>
         <div className="navbar-end">
